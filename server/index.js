21be0aec908b663c6aec4534b101da53265e86fc
bivek <bivekwangkhem6001@gmail.com>
2025-02-14 20:08:32 +0530

    initial commit

--- server/index.js
+++ server/index.js
@@ -1,6 +1,22 @@
 import express from "express"
+// import nodemailer from "nodemailer"
+const app = express();
 
-const app =express();
+
+
+
+app.get("/", async (req, res) => {
+
+
+
+}
+
+)
+
+
+app.listen(9000, () => {
+    console.log("server is listening")
+})
 
 
 
